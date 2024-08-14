a1ab00f0e270f47ce654f1fd9b11b1c857d514eb
Kelison Rosendo da Rocha <kelisonrosendo@gmail.com>
2024-08-14 16:07:07 -0300

    doc: atualizando readme do projeto

--- README.md
+++ README.md
@@ -1,5 +1,103 @@
-# Vue 3 + TypeScript + Vite
+# AutoForm
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+AutoForm é um gerador de formulários dinâmicos, baseado em um objeto de configuração com validação dos campos via zod schema.
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+![AutoForm demo](./src/assets/example.png)
+
+## Tecnologias
+
+O AutoForm foi construído utilizando as seguintes tecnologias:
+
+- [Vue 3](https://vuejs.org/)
+- [Vuetify](https://vuetifyjs.com/)
+- [VeeValidate](https://vee-validate.logaretm.com/v4/)
+- [Zod](https://zod.dev/)
+- [Maska](https://beholdr.github.io/maska/v3/#/vue)
+
+## Instalação
+
+```bash
+# Instalar as dependências
+npm install
+
+# Executar o projeto
+npm run dev
+```
+
+## Tipos de campos
+
+Estes são os tipos de campos suportados até o momento:
+
+- checkbox: VInput, VLabel, VCheckbox
+- input: VTextField
+- radio: VRadioGroup, VRadio
+- select: VSelect,
+- textarea: VTextArea,
+- datePicker: VMenu, VTextField, VDatePicker,
+
+Os tipos disponíveis estão configurados no objeto `FORM_COMPONENTS` dentro de `components/config.ts`.
+
+## Exemplos de uso
+
+Utilização básica:
+
+```vue
+<template>
+  <auto-form
+    v-bind="{ formConfig, formValue }"
+    @update:form-value="updateFormValue"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { z } from "zod";
+import AutoForm from "./components/auto-form.vue";
+import { FormConfig, FormValue } from "./components/types";
+
+// Objeto de configuração para geração do formulário
+const formConfig: FormConfig[] = [
+  {
+    fieldName: "username",
+    fieldType: "input",
+    fieldProps: {
+      label: "Usuário",
+      placeholder: "Insira o usuário",
+    },
+    cols: 6,
+    schema: z
+      .string({
+        required_error: "O usuário é obrigatório",
+      })
+      .min(3, {
+        message: "O usuário deve conter pelo menos 3 caracteres",
+      }),
+  },
+  {
+    fieldName: "email",
+    fieldType: "input",
+    fieldProps: {
+      type: "email",
+      label: "E-mail",
+      placeholder: "Informe o e-mail",
+    },
+    cols: 6,
+    schema: z
+      .string({
+        required_error: "O e-mail é obrigatório",
+      })
+      .email({
+        message: "E-mail inválido",
+      }),
+  },
+];
+
+// Objeto de configuração para valores iniciais dos campos
+const formValue = ref<FormValue<FormConfig[]>>({});
+
+// Função para atualizar os valores do formulário
+const updateFormValue = (newValue: FormValue<FormConfig[]>) => {
+  formValue.value = newValue;
+};
+</script>
+```
--- src/app.vue
+++ src/app.vue
@@ -16,198 +16,42 @@ import AutoForm from "./components/auto-form.vue";
 import { FormConfig, FormValue } from "./components/types";
 
 const formConfig: FormConfig[] = [
-  // {
-  //   fieldName: "money",
-  //   fieldType: "input",
-  //   fieldProps: {
-  //     type: "money",
-  //     label: "Money",
-  //     placeholder: "(#.###,##)",
-  //   },
-  //   schema: z.string(),
-  // },
-  // {
-  //   fieldName: "username",
-  //   fieldType: "input",
-  //   fieldProps: {
-  //     label: "Usuário",
-  //     placeholder: "Insira o usuário",
-  //     mask: "(##) #####-####",
-  //   },
-  //   cols: 6,
-  //   schema: z
-  //     .string({
-  //       required_error: "O usuário é obrigatório",
-  //     })
-  //     .min(2, {
-  //       message: "O usuário deve conter pelo menos  2 caracteres",
-  //     }),
-  // },
-  // {
-  //   fieldName: "password",
-  //   fieldType: "input",
-  //   fieldProps: {
-  //     type: "password",
-  //     label: "Senha",
-  //     placeholder: "Insira sua senha",
-  //   },
-  //   cols: 6,
-  //   schema: z
-  //     .string({
-  //       required_error: "A senha é obrigatória",
-  //     })
-  //     .min(8, { message: "A senha deve conter 8 caracteres" }),
-  // },
-  // {
-  //   fieldName: "email",
-  //   fieldType: "input",
-  //   fieldProps: {
-  //     type: "email",
-  //     label: "E-mail",
-  //     placeholder: "Insira seu e-mail",
-  //   },
-  //   schema: z.string().email({ message: "E-mail inválido" }).optional(),
-  // },
-  // {
-  //   fieldName: "number",
-  //   fieldType: "input",
-  //   fieldProps: {
-  //     type: "number",
-  //     label: "Número",
-  //     placeholder: "Insira um número",
-  //   },
-  //   schema: z.coerce.number({ message: "Somente números" }),
-  // },
-  // {
-  //   fieldName: "textarea",
-  //   fieldType: "textarea",
-  //   fieldProps: {
-  //     label: "Textarea",
-  //     placeholder: "Insira um textão",
-  //     rows: 4,
-  //   },
-  //   schema: z.string().optional(),
-  // },
-  // {
-  //   fieldName: "select",
-  //   fieldType: "select",
-  //   fieldProps: {
-  //     label: "Select simples",
-  //     placeholder: "Selecione uma opção",
-  //     items: [
-  //       {
-  //         id: 1,
-  //         title: "Item 1",
-  //         value: "1",
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Item 2",
-  //         value: 2,
-  //       },
-  //     ],
-  //   },
-  //   cols: 6,
-  //   schema: z.union([
-  //     z.string({ required_error: "Selecione uma opção" }),
-  //     z.number(),
-  //   ]),
-  // },
-  // {
-  //   fieldName: "selectMultiple",
-  //   fieldType: "select",
-  //   fieldProps: {
-  //     label: "Select múltiplo",
-  //     placeholder: "Selecione uma ou mais opções",
-  //     multiple: true,
-  //     returnObject: true,
-  //     items: [
-  //       {
-  //         id: 1,
-  //         title: "Item 1",
-  //         value: "1",
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Item 2",
-  //         value: 2,
-  //       },
-  //     ],
-  //   },
-  //   cols: 6,
-  //   schema: z
-  //     .array(
-  //       z.object({
-  //         id: z.union([z.string(), z.number()]),
-  //         title: z.string(),
-  //         value: z.union([z.string(), z.number()]),
-  //       }),
-  //       { message: "Selecione pelo menos uma opção" }
-  //     )
-  //     .min(1, { message: "Selecione pelo menos uma opção" }),
-  // },
-  // {
-  //   fieldName: "radio",
-  //   fieldType: "radio",
-  //   fieldProps: {
-  //     label: "Radio group",
-  //     items: [
-  //       {
-  //         label: "Item 1",
-  //         value: "1",
-  //       },
-  //       {
-  //         label: "Item 2",
-  //         value: 2,
-  //       },
-  //     ],
-  //   },
-  //   cols: 6,
-  //   schema: z.union([
-  //     z.string({ required_error: "Escolha uma opção" }),
-  //     z.number({ required_error: "Escolha uma opção" }),
-  //   ]),
-  // },
-  // {
-  //   fieldName: "checkbox",
-  //   fieldType: "checkbox",
-  //   fieldProps: {
-  //     label: "Checkbox group",
-  //     items: [
-  //       {
-  //         label: "Item 1",
-  //         value: "1",
-  //       },
-  //       {
-  //         label: "Item 2",
-  //         value: 2,
-  //       },
-  //     ],
-  //   },
-  //   cols: 6,
-  //   schema: z
-  //     .array(z.union([z.string(), z.number()]), {
-  //       required_error: "Selecione ao menos uma opção",
-  //     })
-  //     .min(1, { message: "Selecione ao menos uma opção" }),
-  // },
   {
-    fieldName: "datePicker",
-    fieldType: "datePicker",
+    fieldName: "username",
+    fieldType: "input",
     fieldProps: {
-      label: "DatePicker",
+      label: "Usuário",
+      placeholder: "Insira o usuário",
     },
-    cols: 6,
-    schema: z.date({
-      required_error: "A data é obrigatória",
-      invalid_type_error: "Data inválida",
-    }),
+    cols: 12,
+    schema: z
+      .string({
+        required_error: "O usuário é obrigatório",
+      })
+      .min(2, {
+        message: "O usuário deve conter pelo menos 2 caracteres",
+      }),
+  },
+  {
+    fieldName: "email",
+    fieldType: "input",
+    fieldProps: {
+      type: "email",
+      label: "E-mail",
+      placeholder: "Insira o e-mail",
+    },
+    cols: 12,
+    schema: z
+      .string({
+        required_error: "A senha é obrigatória",
+      })
+      .email({
+        message: "E-mail inválido",
+      }),
   },
 ];
 
-const formValue = ref<FormValue<FormConfig[]>>({
-  datePicker: new Date(),
-});
+const formValue = ref<FormValue<FormConfig[]>>({});
 
 const updateFormValue = (newValue: FormValue<FormConfig[]>) => {
   formValue.value = newValue;
